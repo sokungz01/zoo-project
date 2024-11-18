c0dd6757d24987b8f8f8a6afe6d3c08cf584a0a6
Panurut Sirinapaisan <panurut.siri@kmutt.ac.th>
2024-11-19 01:14:29 +0700

    fix: zoo ticket price condition and unit-test

--- test_zoo.py
+++ test_zoo.py
@@ -5,10 +5,21 @@ class TestZoo(unittest.TestCase):
     def setUp(self):
         self.zoo = Zoo()
 
-    def test_child_ticket_price(self):
-        self.assertEqual(self.zoo.get_ticket_price(5), 50)
-       
-    # Add your additional test cases here.
+    def test_get_ticket_price_age_0(self):
+        self.assertEqual(self.zoo.get_ticket_price(-1), 0)
+
+    def test_get_ticket_price_age_12(self):
+        self.assertEqual(self.zoo.get_ticket_price(12), 50)
+
+    def test_get_ticket_price_age_20(self):
+        self.assertEqual(self.zoo.get_ticket_price(20), 100)
+
+    def test_get_ticket_price_age_21(self):
+        self.assertEqual(self.zoo.get_ticket_price(21), 150)
+    
+    def test_get_ticket_price_age_61(self):
+        self.assertEqual(self.zoo.get_ticket_price(61), 100)
+
 
 if __name__ == '__main__':
     unittest.main()
